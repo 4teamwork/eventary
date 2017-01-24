d8fbcc08297db9e4bf00cf610d25290390179053
Pablo Vergés <pablo.verges@4teamwork.ch>
2017-01-24 14:18:32 +0100

    Refactors IndexView

--- views/index.py
+++ views/index.py
@@ -11,12 +11,29 @@ class IndexView(TemplateView):
 
     template_name = 'eventary/index.html'
 
+    def filter_qs(self, qs):
+
+        if self.form.is_valid():
+            data = self.form.clean()
+            if data['from_date'] is not None:
+                qs = qs.exclude(
+                    eventtimedate__start_date__lt=data['from_date']
+                )
+            if data['to_date'] is not None:
+                qs = qs.exclude(
+                    eventtimedate__start_date__gt=data['to_date']
+                )
+            groups = self.form.groups()
+            if len(groups) > 0:
+                qs = qs.filter(group__in=groups)
+        return qs
+
     def get_form(self):
         if len(self.request.GET):
-            form = GenericFilterForm(self.request.GET, prefix="filter")
+            self.form = GenericFilterForm(self.request.GET, prefix="filter")
         else:
-            form = GenericFilterForm(prefix="filter")
-        return form
+            self.form = GenericFilterForm(prefix="filter")
+        return self.form
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
@@ -45,50 +62,18 @@ class IndexView(TemplateView):
         context.update({
             'form': form
         })
-        if form.is_valid():
-            data = form.clean()
-            if data['from_date'] is not None:
-                event_list = event_list.exclude(
-                    eventtimedate__start_date__lt=data['from_date']
-                )
-                proposal_list = proposal_list.exclude(
-                    eventtimedate__start_date__lt=data['from_date']
-                )
-            if data['to_date'] is not None:
-                event_list = event_list.exclude(
-                    eventtimedate__start_date__gt=data['to_date']
-                )
-                proposal_list = proposal_list.exclude(
-                    eventtimedate__start_date__gt=data['to_date']
-                )
-            groups = form.groups()
-            if len(groups) > 0:
-                event_list = event_list.filter(group__in=groups)
-                proposal_list = proposal_list.filter(group__in=groups)
+        event_list = self.filter_qs(event_list)
+        proposal_list = self.filter_qs(proposal_list)
 
-        # create some paginators for the event lists
-        event_paginator = Paginator(event_list, 25)
-        proposal_paginator = Paginator(proposal_list, 25)
-
-        # paginate the events [!! overrides event_list !!]
-        event_page = self.request.GET.get('event_page', 1)
-        try:
-            event_list = event_paginator.page(event_page)
-        except PageNotAnInteger:
-            event_list = event_paginator.page(1)
-        except EmptyPage:
-            event_list = event_paginator.page(event_paginator.num_pages)
-
-        # paginate the proposals [!! overrides proposal_list !!]
-        proposal_page = self.request.GET.get('proposal_page', 1)
-        try:
-            proposal_list = proposal_paginator.page(proposal_page)
-        except PageNotAnInteger:
-            proposal_list = proposal_paginator.page(1)
-        except EmptyPage:
-            proposal_list = proposal_paginator.page(
-                proposal_paginator.num_pages
-            )
+        # create some paginators
+        event_list, event_paginator = self.paginate_qs(
+            event_list,
+            prefix='event'
+        )
+        proposal_list, proposal_paginator = self.paginate_qs(
+            proposal_list,
+            prefix='proposal'
+        )
 
         context.update({
             'event_list':    event_list,
@@ -101,3 +86,16 @@ class IndexView(TemplateView):
         })
 
         return context
+
+    def paginate_qs(self, qs, prefix='paginator'):
+        paginator = Paginator(qs, 25)
+
+        page = self.request.GET.get('{0}_page'.format(prefix), 1)
+        try:
+            obj_list = paginator.page(page)
+        except PageNotAnInteger:
+            obj_list = paginator.page(1)
+        except EmptyPage:
+            obj_list = paginator.page(paginator.num_pages)
+
+        return obj_list, paginator
