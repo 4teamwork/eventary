065162b3b2c6a2962c8463a1ab8cc62a04b9fd54
Pablo Vergés <pablo.verges@4teamwork.ch>
2017-01-24 11:35:59 +0100

    Paginating the lists of events and proposals!
    - adds paginators for the list of events and proposals
    - also improves the position of the message to add further proposals

--- views/index.py
+++ views/index.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import TemplateView
 
 from ..models import Calendar, Event, EventTimeDate
@@ -13,23 +15,55 @@ class IndexView(TemplateView):
 
         # general context data
         context.update({
-            'calendar_list': Calendar.objects.all(),
-            'event_list':    Event.objects.filter(published=True),
-            'proposal_list': Event.objects.filter(published=False),
-            'timedate_list': EventTimeDate.objects.all()
+            'calendar_list':  Calendar.objects.all(),
+            'event_count':    Event.objects.filter(published=True).count(),
+            'proposal_count': Event.objects.filter(published=False).count(),
+            'timedate_count': EventTimeDate.objects.count()
         })
 
         # upcoming events and proposals
         today = datetime.today()
+        event_list = Event.objects.filter(
+            published=True,
+            eventtimedate__start_date__gte=today,
+        ).distinct()
+        proposal_list = Event.objects.filter(
+            published=False,
+            eventtimedate__start_date__gte=today,
+        ).distinct()
+
+        # create some paginators for the event lists
+        event_paginator = Paginator(event_list, 25)
+        proposal_paginator = Paginator(proposal_list, 25)
+
+        # paginate the events [!! overrides event_list !!]
+        event_page = self.request.GET.get('event_page', 1)
+        try:
+            event_list = event_paginator.page(event_page)
+        except PageNotAnInteger:
+            event_list = event_paginator.page(1)
+        except EmptyPage:
+            event_list = event_paginator.page(event_paginator.num_pages)
+
+        # paginate the proposals [!! overrides proposal_list !!]
+        proposal_page = self.request.GET.get('proposal_page', 1)
+        try:
+            proposal_list = proposal_paginator.page(proposal_page)
+        except PageNotAnInteger:
+            proposal_list = proposal_paginator.page(1)
+        except EmptyPage:
+            proposal_list = proposal_paginator.page(
+                proposal_paginator.num_pages
+            )
+
         context.update({
-            'upcoming_event_list': Event.objects.filter(
-                published=True,
-                eventtimedate__start_date__gte=today,
-            ).distinct(),
-            'upcoming_proposal_list': Event.objects.filter(
-                published=False,
-                eventtimedate__start_date__gte=today,
-            ).distinct()
+            'event_list':    event_list,
+            'proposal_list': proposal_list
+        })
+
+        context.update({
+            'event_paginator': event_paginator,
+            'proposal_paginator': proposal_paginator
         })
 
         return context
